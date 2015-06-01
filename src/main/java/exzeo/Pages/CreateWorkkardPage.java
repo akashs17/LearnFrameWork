656838350de1406fb437134163ce6fcf12511000
Akash <akashs@exzeo.com>
2015-06-01 18:35:28 +0530

    ReportNG Changes

--- src/main/java/exzeo/Pages/CreateWorkkardPage.java
+++ src/main/java/exzeo/Pages/CreateWorkkardPage.java
@@ -1,28 +1,25 @@
 package exzeo.Pages;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+
 import java.util.Date;
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.interactions.Keyboard;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Reporter;
+
 
 import exzeo.Configuration.Constants;
 
@@ -384,12 +381,9 @@ public class CreateWorkkardPage {
 		
 	public WorkkardDetailViewPage createDefaultWorkkard(String workkardNumber){
 		
-		Reporter.log("Before to enter workard number");
-		enterWorkkardNumber(workkardNumber);
 		
-		Reporter.log("After enter workard number");
-		enterWorkkardSummary("This is a default workkard");
-		
+		enterWorkkardNumber(workkardNumber);			
+		enterWorkkardSummary("This is a default workkard");		
 		clickOnSaveButton();
 		return new WorkkardDetailViewPage(driver);
 	}
